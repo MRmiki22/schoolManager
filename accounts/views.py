221b5228e1515d0ebd5380476038bdf9b03c6b05
mgeza <mgeza@ZOO>
2023-08-11 18:25:48 +0300

    templates and view are created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# accounts/views.py
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import LoginForm, LogoutForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('login')  # Redirect to appropriate page
+            else:
+                # Invalid credentials
+                form.add_error(None, "Invalid username or password")
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def user_logout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')  # Redirect to login page
+    else:
+        form = LogoutForm()
+    return render(request, 'logout.html', {'form': form})
+
